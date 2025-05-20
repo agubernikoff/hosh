eec0b4ca09527b32234a8142a4e1028d5344e43f
alexander gubernikoff <agubernikoff@gmail.com>
2025-05-19 22:22:05 -0400

    init: homepage

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,8 +1,9 @@
-import {Await, useLoaderData, Link} from '@remix-run/react';
+import {Await, useLoaderData, Link, NavLink} from '@remix-run/react';
 import {Suspense} from 'react';
 import {Image} from '@shopify/hydrogen';
 import {ProductItem} from '~/components/ProductItem';
-
+import model1 from '~/assets/model1.png';
+import model2 from '~/assets/model2.png';
 /**
  * @type {MetaFunction}
  */
@@ -29,13 +30,15 @@ export async function loader(args) {
  * @param {LoaderFunctionArgs}
  */
 async function loadCriticalData({context}) {
-  const [{collections}] = await Promise.all([
-    context.storefront.query(FEATURED_COLLECTION_QUERY),
+  const [{collection}] = await Promise.all([
+    context.storefront.query(NEW_ARRIVALS_QUERY, {
+      variables: {handle: 'craig-george'},
+    }),
     // Add other queries here, so that they are loaded in parallel
   ]);
 
   return {
-    featuredCollection: collections.nodes[0],
+    featuredCollection: collection,
   };
 }
 
@@ -78,18 +81,57 @@ export default function Homepage() {
 function FeaturedCollection({collection}) {
   if (!collection) return null;
   const image = collection?.image;
+  console.log(collection);
   return (
-    <Link
-      className="featured-collection"
-      to={`/collections/${collection.handle}`}
-    >
-      {image && (
-        <div className="featured-collection-image">
-          <Image data={image} sizes="100vw" />
+    <div className="featured-artist-container">
+      <div className="featured-artist-homepage-section">
+        <div className="models-container">
+          <img src={model1} />
+          <img src={model2} />
         </div>
-      )}
-      <h1>{collection.title}</h1>
-    </Link>
+        <div className="shop-the-collection">
+          <p>FEATURED ARTIST</p>
+          <p className="artist-title">{collection.title.toUpperCase()}</p>
+          <NavLink to={`/collections/${collection.handle}`}>
+            SHOP THE COLLECTION
+          </NavLink>
+        </div>
+      </div>
+      <Link
+        className="featured-collection"
+        to={`/collections/${collection.handle}`}
+      >
+        {image && (
+          <div className="featured-collection-image">
+            <Image data={image} sizes="100vw" />
+          </div>
+        )}
+      </Link>
+      <p
+        style={{marginBlock: '3rem', textAlign: 'center'}}
+      >{`${collection.title.toUpperCase()} COLLECTION`}</p>
+      <div className="recommended-products-grid">
+        {collection.products.nodes.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+      <div
+        style={{
+          marginBlock: '5rem',
+        }}
+      >
+        <NavLink
+          to={`/collections/${collection.handle}`}
+          style={{
+            padding: '1rem',
+            border: '1px solid black',
+            boxSizing: 'border-box',
+          }}
+        >
+          SHOP THE COLLECTION
+        </NavLink>
+      </div>
+    </div>
   );
 }
 
@@ -101,7 +143,7 @@ function FeaturedCollection({collection}) {
 function RecommendedProducts({products}) {
   return (
     <div className="recommended-products">
-      <h2>Recommended Products</h2>
+      <p style={{marginBlock: '3rem', textAlign: 'center'}}>BEST SELLERS</p>
       <Suspense fallback={<div>Loading...</div>}>
         <Await resolve={products}>
           {(response) => (
@@ -120,31 +162,8 @@ function RecommendedProducts({products}) {
   );
 }
 
-const FEATURED_COLLECTION_QUERY = `#graphql
-  fragment FeaturedCollection on Collection {
-    id
-    title
-    image {
-      id
-      url
-      altText
-      width
-      height
-    }
-    handle
-  }
-  query FeaturedCollection($country: CountryCode, $language: LanguageCode)
-    @inContext(country: $country, language: $language) {
-    collections(first: 1, sortKey: UPDATED_AT, reverse: true) {
-      nodes {
-        ...FeaturedCollection
-      }
-    }
-  }
-`;
-
-const RECOMMENDED_PRODUCTS_QUERY = `#graphql
-  fragment RecommendedProduct on Product {
+const PRODUCT_ITEM_FRAGMENT = `#graphql
+  fragment ProductItem on Product {
     id
     title
     handle
@@ -156,20 +175,84 @@ const RECOMMENDED_PRODUCTS_QUERY = `#graphql
     }
     featuredImage {
       id
-      url
       altText
+      url
       width
       height
     }
-  }
-  query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
-    @inContext(country: $country, language: $language) {
-    products(first: 4, sortKey: UPDATED_AT, reverse: true) {
+    images(first: 2) {
       nodes {
-        ...RecommendedProduct
+        id
+        url
+        altText
+        width
+        height
       }
     }
   }
+`;
+
+const NEW_ARRIVALS_QUERY = `#graphql
+  ${PRODUCT_ITEM_FRAGMENT}
+  query Collection(
+    $country: CountryCode
+    $language: LanguageCode
+    $handle: String
+  ) @inContext(country: $country, language: $language) {
+    collection(handle: $handle) {
+      id
+      handle
+      title
+      description
+      image {
+        id
+        url
+        altText
+        width
+        height
+      }
+      products(
+        first: 3
+      ) {
+        filters{
+          id
+          label
+          presentation
+          type
+          values{
+            count
+            id
+            input
+            label
+            swatch{
+              color
+            }
+          }
+        }
+        nodes {
+          ...ProductItem
+        }
+        pageInfo {
+          hasPreviousPage
+          hasNextPage
+          endCursor
+          startCursor
+        }
+      }
+    }
+  }
+`;
+
+const RECOMMENDED_PRODUCTS_QUERY = `#graphql
+  query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
+    @inContext(country: $country, language: $language) {
+    products(first: 9, sortKey: BEST_SELLING, reverse: true) {
+      nodes {
+        ...ProductItem
+      }
+    }
+  }
+  ${PRODUCT_ITEM_FRAGMENT}
 `;
 
 /** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
