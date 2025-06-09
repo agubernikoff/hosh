e72bb67da1f587e9b18796629e26be8568a97805
alexander gubernikoff <agubernikoff@gmail.com>
2025-06-09 08:38:02 -0400

    init header dropdowns

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,6 +6,8 @@ import logo from '../assets/Group 196.png';
 import search from '../assets/search.png';
 import bag from '../assets/bag.png';
 import acct from '../assets/acct.png';
+import {useState} from 'react';
+import {motion, AnimatePresence} from 'motion/react';
 
 /**
  * @param {HeaderProps}
@@ -48,6 +50,7 @@ export function HeaderMenu({
   const className = `header-menu-${viewport}`;
   const {close} = useAside();
 
+  const [open, setOpen] = useState();
   return (
     <nav className={className} role="navigation">
       {viewport === 'mobile' && (
@@ -63,6 +66,7 @@ export function HeaderMenu({
       )}
       {(menu || FALLBACK_HEADER_MENU).items.map((item) => {
         if (!item.url) return null;
+        console.log(item);
 
         // if the url is internal, we strip the domain
         const url =
@@ -71,7 +75,39 @@ export function HeaderMenu({
           item.url.includes(primaryDomainUrl)
             ? new URL(item.url).pathname
             : item.url;
-        return (
+        return item.items.length > 0 ? (
+          <Dropdown label={item.title} key={item.id}>
+            {item.items.map((item) => {
+              if (!item.url) return null;
+              console.log(item);
+
+              // if the url is internal, we strip the domain
+              const url =
+                item.url.includes('myshopify.com') ||
+                item.url.includes(publicStoreDomain) ||
+                item.url.includes(primaryDomainUrl)
+                  ? new URL(item.url).pathname
+                  : item.url;
+              return item.items.length > 0 ? (
+                <Dropdown label={item.title} key={item.id} displaySVG={true}>
+                  x
+                </Dropdown>
+              ) : (
+                <NavLink
+                  className="header-menu-item"
+                  end
+                  key={item.id}
+                  onClick={close}
+                  prefetch="intent"
+                  style={activeLinkStyle}
+                  to={url}
+                >
+                  {item.title}
+                </NavLink>
+              );
+            })}
+          </Dropdown>
+        ) : (
           <NavLink
             className="header-menu-item"
             end
@@ -86,6 +122,63 @@ export function HeaderMenu({
         );
       })}
     </nav>
+  );
+}
+
+function Dropdown({children, label, displaySVG, isOpen}) {
+  const [isOpenNested, setIsOpenNested] = useState(false);
+  function toggleIsOpen() {
+    setIsOpenNested(!isOpenNested);
+  }
+  return (
+    <button
+      className={`mobile-filter ${isOpenNested ? 'isOpen-btn' : ''} header-mf header-menu-item`}
+      onClick={toggleIsOpen}
+    >
+      <AnimatePresence mode="popLayout">
+        <motion.span
+          key={`filt-by-${isOpenNested}`}
+          initial={{opacity: 1}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          style={{display: 'inline-block', textAlign: 'left'}}
+        >
+          {label}
+        </motion.span>
+      </AnimatePresence>
+      {displaySVG ? (
+        <motion.svg
+          width="8"
+          height="4"
+          viewBox="0 0 8 4"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          initial={{rotate: 0}}
+          animate={{rotate: isOpenNested ? '180deg' : 0}}
+          transition={{ease: 'easeInOut', duration: 0.15}}
+        >
+          <path
+            d="M7.82391 0.152959C7.71112 0.0550192 7.55817 -4.38211e-07 7.39869 -4.24269e-07C7.23921 -4.10327e-07 7.08626 0.0550192 6.97348 0.152959L3.99637 2.73897L1.01926 0.15296C0.905829 0.0577954 0.753904 0.00513784 0.59621 0.00632804C0.438515 0.00751848 0.287667 0.0624613 0.176156 0.159324C0.064645 0.256186 0.00139348 0.387217 2.30612e-05 0.524196C-0.00134688 0.661175 0.0592744 0.79314 0.168831 0.891671L3.57115 3.84704C3.68394 3.94498 3.83689 4 3.99637 4C4.15585 4 4.3088 3.94498 4.42158 3.84704L7.82391 0.891671C7.93666 0.793701 8 0.660844 8 0.522315C8 0.383786 7.93666 0.250929 7.82391 0.152959Z"
+            fill="black"
+          />
+        </motion.svg>
+      ) : null}
+
+      <AnimatePresence>
+        {isOpenNested && (
+          <div className="sort-overflow-hidden-container header-sohc">
+            <motion.div
+              initial={{y: '-100%'}}
+              animate={{y: '1px'}}
+              exit={{y: '-100%'}}
+              transition={{ease: 'easeInOut', duration: 0.15}}
+            >
+              <div className="sort-container header-s-c">{children}</div>
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
+    </button>
   );
 }
 
--- app/lib/fragments.js
+++ app/lib/fragments.js
@@ -171,8 +171,14 @@ const MENU_FRAGMENT = `#graphql
     type
     url
   }
+  fragment GrandChildMenuItem on MenuItem {
+    ...MenuItem
+  }
   fragment ChildMenuItem on MenuItem {
     ...MenuItem
+    items{
+      ...GrandChildMenuItem
+    }
   }
   fragment ParentMenuItem on MenuItem {
     ...MenuItem
