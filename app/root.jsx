ef26eb2e15190a5109352913033fee90356abbf9
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-08-29 12:26:27 -0400

    meta pixel

--- app/root.jsx
+++ app/root.jsx
@@ -191,6 +191,18 @@ export function Layout({children}) {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="stylesheet" href={resetStyles}></link>
         <link rel="stylesheet" href={appStyles}></link>
+        <script
+          nonce={nonce}
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K6RRFC7V');
+        `,
+          }}
+        />
         <Meta />
         <Links />
         {/* Heatmap Snippet */}
@@ -217,56 +229,22 @@ export function Layout({children}) {
         />
       </head>
       <body>
-        <script
-          nonce={nonce}
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-11512005692"
-        ></script>
-        <script
-          nonce={nonce}
-          dangerouslySetInnerHTML={{
-            __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'AW-11512005692');
-    `,
-          }}
-        ></script>
-        <script
-          nonce={nonce}
-          dangerouslySetInnerHTML={{
-            __html: `
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;
-      n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', 'YOUR_PIXEL_ID');
-      fbq('track', 'PageView');
-    `,
-          }}
-        />
         <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1"
-            alt=""
-          />
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K6RRFC7V"
+            height="0"
+            width="0"
+            style={{display: 'none', visibility: 'hidden'}}
+            title="GTM"
+          ></iframe>
         </noscript>
-        <ClientTracker />
         {data ? (
           <Analytics.Provider
             cart={data.cart}
             shop={data.shop}
             consent={data.consent}
           >
+            <ClientTracker />
             <PageLayout {...data}>{children}</PageLayout>
           </Analytics.Provider>
         ) : (
