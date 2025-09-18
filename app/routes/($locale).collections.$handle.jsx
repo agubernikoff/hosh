4eac5b06c9452d70ff68de5cbc1df26b0428be23
alexander gubernikoff <agubernikoff@gmail.com>
2025-09-18 09:03:05 +0900

    overscroll mobiel filter

--- app/routes/($locale).collections.$handle.jsx
+++ app/routes/($locale).collections.$handle.jsx
@@ -454,7 +454,6 @@ function SelectedFilters({selectedFilters, removeFilter, clearFilter}) {
 
 function Sort({addSort, removeSort, isChecked, term, shopAll}) {
   const [isOpen, setIsOpen] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
   const sortRef = useRef(null);
 
   useEffect(() => {
@@ -531,14 +530,7 @@ function Sort({addSort, removeSort, isChecked, term, shopAll}) {
   }
 
   return (
-    <button
-      onClick={toggleIsOpen}
-      className="sort-by-button"
-      ref={sortRef}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      style={isHovered ? {fontWeight: 500} : {}}
-    >
+    <button onClick={toggleIsOpen} className="sort-by-button" ref={sortRef}>
       <span>
         <span style={{display: 'inline-block', textAlign: 'left'}}>
           {`Sort: `}
@@ -704,7 +696,6 @@ function Filt({
 
 // Modified MobileFilt component to handle cascading behavior
 function MobileFilt({children, label, isOpen, onToggle, parentOpen}) {
-  const [isHovered, setIsHovered] = useState(false);
   const [isMobile, setIsMobile] = useState(
     typeof window !== 'undefined' ? window.innerWidth <= 700 : true,
   );
@@ -733,42 +724,37 @@ function MobileFilt({children, label, isOpen, onToggle, parentOpen}) {
   }, [parentOpen]); // Only depend on parentOpen, not isOpen or onToggle
 
   return (
-    <>
-      <button
-        className={`mobile-filter ${isOpen ? 'isOpen-btn' : ''}`}
-        onClick={onToggle}
-        style={isHovered ? {fontWeight: 500} : {}}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <AnimatePresence mode="popLayout">
-          <motion.span
-            key={`filt-by-${isOpen}`}
-            initial={{opacity: 1}}
-            animate={{opacity: 1}}
-            exit={{opacity: 0}}
-            style={{display: 'inline-block', textAlign: 'left'}}
-          >
-            {label}
-          </motion.span>
-        </AnimatePresence>
-        <motion.svg
-          width="8"
-          height="4"
-          viewBox="0 0 8 4"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          initial={{rotate: 0}}
-          animate={{rotate: isOpen ? '180deg' : 0}}
-          transition={{ease: 'easeInOut', duration: 0.15}}
+    <button
+      className={`mobile-filter ${isOpen ? 'isOpen-btn' : ''}`}
+      onClick={onToggle}
+    >
+      <AnimatePresence mode="popLayout">
+        <motion.span
+          key={`filt-by-${isOpen}`}
+          initial={{opacity: 1}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          style={{display: 'inline-block', textAlign: 'left'}}
         >
-          <path
-            d="M7.82391 0.152959C7.71112 0.0550192 7.55817 -4.38211e-07 7.39869 -4.24269e-07C7.23921 -4.10327e-07 7.08626 0.0550192 6.97348 0.152959L3.99637 2.73897L1.01926 0.15296C0.905829 0.0577954 0.753904 0.00513784 0.59621 0.00632804C0.438515 0.00751848 0.287667 0.0624613 0.176156 0.159324C0.064645 0.256186 0.00139348 0.387217 2.30612e-05 0.524196C-0.00134688 0.661175 0.0592744 0.79314 0.168831 0.891671L3.57115 3.84704C3.68394 3.94498 3.83689 4 3.99637 4C4.15585 4 4.3088 3.94498 4.42158 3.84704L7.82391 0.891671C7.93666 0.793701 8 0.660844 8 0.522315C8 0.383786 7.93666 0.250929 7.82391 0.152959Z"
-            fill="black"
-          />
-        </motion.svg>
-      </button>
-      <AnimatePresence>
+          {label}
+        </motion.span>
+      </AnimatePresence>
+      <motion.svg
+        width="8"
+        height="4"
+        viewBox="0 0 8 4"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        initial={{rotate: 0}}
+        animate={{rotate: isOpen ? '180deg' : 0}}
+        transition={{ease: 'easeInOut', duration: 0.15}}
+      >
+        <path
+          d="M7.82391 0.152959C7.71112 0.0550192 7.55817 -4.38211e-07 7.39869 -4.24269e-07C7.23921 -4.10327e-07 7.08626 0.0550192 6.97348 0.152959L3.99637 2.73897L1.01926 0.15296C0.905829 0.0577954 0.753904 0.00513784 0.59621 0.00632804C0.438515 0.00751848 0.287667 0.0624613 0.176156 0.159324C0.064645 0.256186 0.00139348 0.387217 2.30612e-05 0.524196C-0.00134688 0.661175 0.0592744 0.79314 0.168831 0.891671L3.57115 3.84704C3.68394 3.94498 3.83689 4 3.99637 4C4.15585 4 4.3088 3.94498 4.42158 3.84704L7.82391 0.891671C7.93666 0.793701 8 0.660844 8 0.522315C8 0.383786 7.93666 0.250929 7.82391 0.152959Z"
+          fill="black"
+        />
+      </motion.svg>
+      <AnimatePresence mode="wait">
         {isOpen && (
           <div
             className="sort-overflow-hidden-container"
@@ -796,7 +782,7 @@ function MobileFilt({children, label, isOpen, onToggle, parentOpen}) {
           </div>
         )}
       </AnimatePresence>
-    </>
+    </button>
   );
 }
 
